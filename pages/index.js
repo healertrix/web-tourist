f65059d06dcb91d87a910371f9d1bdb193e59778
Amartya Nambiar <amartyanambiar@gmail.com>
2022-06-18 22:43:24 +0530

    nav setup

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import Navbar from "../components/navbar";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">Tourist App web!</h1>
+    <div className="text-center bg-gray-50">
+      <Navbar></Navbar>
+    </div>
   );
 }
